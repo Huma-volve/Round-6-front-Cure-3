16fc9a7213e70d35e82dc17ffc4a96ef3c578cf4
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2025-09-16 22:10:37 +0300

    fix(home): fix responsive styling for install app section

--- src/features/Home/SectionApp.tsx
+++ src/features/Home/SectionApp.tsx
@@ -7,39 +7,51 @@ const SectionApp = () => {
   return (
     <section
       id="app"
-      className="bg-Background-Primary-Lighter rounded-[20px] mb-[-50px] z-10 relative py-10 px-16 flex gap-8"
+      className="bg-Background-Primary-Lighter rounded-[20px] mb-[-50px] z-10 relative py-5 lg:py-10 px-8 lg:px-16 flex md:flex-row flex-col gap-9 md:gap-0 items-center md:justify-between"
     >
       <div className="text-white max-w-[527px]">
-        <h2 className="text-4xl mb-4">Your Health, One Tap Away</h2>
-        <p className="text-base mb-[60px]">
+        <h2 className="text-3xl md:text-4xl mb-4">Your Health, One Tap Away</h2>
+        <p className="text-sm md:text-base md:mb-[60px] mb-8">
           Book appointments, chat with doctors, and manage your health
           anytime—right from your phone. Download the app now and stay connected
           wherever you are.
         </p>
-        <div className="flex gap-10">
+        <div className="flex sm:flex-row flex-col gap-3 lg:gap-10">
           <a
             href="#"
-            className="flex gap-2 items-center bg-Background-Secondary-Defult py-2 px-4 rounded-[8px]"
+            className="flex w-fit gap-2 items-center bg-Background-Secondary-Defult py-2 px-4 rounded-[8px]"
           >
-            <img src={googlePlayIcon} alt="google play icon" />{" "}
+            <img
+              className="w-6 lg:w-8"
+              src={googlePlayIcon}
+              alt="google play icon"
+            />{" "}
             <p className="text-xs flex flex-col ">
               Got IT ON
-              <span className="font-medium text-[20px]">Google Play</span>
+              <span className="font-medium text-base lg:text-[20px]">
+                Google Play
+              </span>
             </p>
           </a>
           <a
             href="#"
-            className="flex gap-2 items-center bg-Background-Secondary-Defult py-2 px-4 rounded-[8px]"
+            className="flex w-fit gap-2 items-center bg-Background-Secondary-Defult lg:py-2 py-1 lg:px-4 px-2 rounded-[8px]"
           >
-            <img src={appleIcon} alt="apple icon" />{" "}
+            <img className="w-6 lg:w-8" src={appleIcon} alt="apple icon" />{" "}
             <p className="text-xs flex flex-col">
               Download on the
-              <span className="font-medium text-[20px]">Apple Store</span>
+              <span className="font-medium text-base lg:text-[20px]">
+                Apple Store
+              </span>
             </p>
           </a>
         </div>
       </div>
-      <img className="mx-auto" src={phoneImg} alt="2 phones" />
+      <img
+        className="max-w-full h-auto object-contain"
+        src={phoneImg}
+        alt="2 phones"
+      />
     </section>
   );
 };
