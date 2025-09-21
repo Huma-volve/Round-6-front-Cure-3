1c670863b2624ac77660bc1d46678a37bef5b7ca
khaledhany100 <hanykhaled153@gmail.com>
2025-09-21 21:23:55 +0300

    edit api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-const TOKEN = "3|7gKZsNspIPXDG7HdG0ndcxN6gMLZQdh4lTt7sn9h96a5e0e3";
+const TOKEN = localStorage.getItem("token") || "";
 export const api = axios.create({
-    baseURL: "http://round5-online-booking-with-doctor-api.huma-volve.com/api",
+    baseURL: "https://round5-online-booking-with-doctor-api.huma-volve.com/api",
     headers: {
         Authorization: `Bearer ${TOKEN}`,
         "Content-Type": "application/json",
